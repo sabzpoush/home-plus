d854be7c50733f05867a51a18736bbe03d8351f1
amir sabz <amirsabz84@gmail.com>
2024-09-14 21:57:36 +0330

    add buyer type

---
+++ src/graphql/types/buyer.type.ts
@@ -0,0 +1,26 @@
+export const buyer = `#graphql
+    type Buyer{
+        id:String!
+        title:String!
+        price:Int
+        sellerName:String
+        phone:String!
+        address:String
+        detail:String
+        fromMeter:Int
+        toMeter:Int
+        room:Int
+        fromBuildYear:Int
+        toBuildYear:Int
+        floor:Int
+        countFloor:Int
+        parking:Boolean
+        elvator:Boolean
+        warehouse:Boolean
+        tag:[String]
+        submitedAt:String!
+        type:BuyerType
+        property:[BuyerRequestType]
+        user:User
+    }
+`;
