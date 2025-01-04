3a2a20e46ca1ecd38efab9d1202c94a8b87f4a4a
amir sabz <50959230+sabzpoush@users.noreply.github.com>
2025-01-04 18:35:11 +0330

    update util
    update auth.util and add verify user

--- src/utils/auth/auth.util.ts
+++ src/utils/auth/auth.util.ts
@@ -2,6 +2,8 @@ import { PrismaClient, User } from '@prisma/client';
 const prisma = new PrismaClient();
 import jwt from 'jsonwebtoken';
 
+
+// validate the user token and check the access
 export async function userValidator(req):Promise<User>{
     const key = process.env['SECRET_KEY'];
 
@@ -26,5 +28,41 @@ export async function userValidator(req):Promise<User>{
         }
     }else{
         throw new Error("Cant authoriz!");
+    }
+}
+
+// check the user token and validate it
+export async function verifyUserToken(req):Promise<boolean>{
+    try{
+        const key = process.env['SECRET_KEY'];
+
+        if(req){
+            const authorization = req.headers.authorization;
+            const authHeader = authorization.split(" ")[1];
+
+            if(authHeader){
+                try{
+                    const verifyToken = jwt.verify(authHeader,key);
+                    if(!verifyToken){
+                        return false;
+                    }
+                }catch(e){
+                    return false;
+                }
+                const {email} = jwt.decode(authHeader) as any;
+
+                const user = await prisma.user.findUnique({where:{email}});
+                if(!user){
+                    return false;
+                }
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+           return false;
+        }
+    }catch(e){
+        return false;
     }
 }
