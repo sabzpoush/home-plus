f670b2acc36cd1b0005789d93caf189c09c9832a
amir sabz <50959230+sabzpoush@users.noreply.github.com>
2025-01-22 21:50:56 +0330

    Create house.resolver.ts

---
+++ src/graphql/resolvers/house.resolver.ts
@@ -0,0 +1,161 @@
+import {PrismaClient,User,Sale} from '@prisma/client'
+const prisma = new PrismaClient();
+import {userValidator} from '../../utils/auth/auth.util';
+import { houseFilter, saleFilter } from '../../utils/helper/filter';
+
+
+export const houseMutation = {
+    submitHouse:async(_,{house:args},context)=>{
+        const user:User = await userValidator(context.req);
+        
+        const house = await prisma.house.create({data:{userId:user.id,...args}});
+        if(!house){
+            throw new Error("در ثبت این اگهی مشکلی رخ داد!");
+        }
+
+        return house;
+    },
+    editHouse:async(_,{houseId,house:args},context)=>{
+        const user:User = await userValidator(context.req);
+
+        const house = await prisma.house.update({
+            where:{id:houseId,userId:user.id},
+            data:{...args}
+        });
+
+        const updatedHouse = await prisma.house.findUnique({where:{id:house.id}});
+        if(!updatedHouse){
+            throw new Error(
+                "در بروزرسانی ملک مشکلی بوجود امد!"
+            );
+        }
+
+        return updatedHouse;
+    },
+    filterHouseByParameters:async(_,{filter},context)=>{
+        const {
+            room,
+            priceFrom = 0,
+            priceTo,
+            buildYearFrom = 0,
+            buildYearTo = 9999,
+            meterageFrom = 0,
+            meterageTo = 999999,
+            category=["Rent","Asker","Buyer","Sale"],
+            type=[
+                "Apartment",
+                "Pilot",
+                "Basement",
+                "Land",
+                "Resident",
+                "Buyer",
+                "Rent"
+            ],
+        } = filter;
+
+        const house = await prisma.house.findMany({
+            where:{
+                AND:[
+                    {category:{in:category}},
+                    {...(room !== undefined && { room:{gte:room}})},
+                    {...(priceFrom !== undefined &&{price:{gte:priceFrom}})},
+                    {...(priceTo !== undefined &&{price:{lte:priceTo}})},
+                    {buildYear:{gte:buildYearFrom,lte:buildYearTo}},
+                    {meterage:{gte:meterageFrom,lte:meterageTo}},
+                    {type:{in:type}},
+                ]
+            }
+        });
+
+        if(house.length  == 0) throw new Error('ملکی مشابه نیاز های شما یافت نشد!');
+
+        return house;
+    },
+    selfHouseFilter:async(_,{filter},context)=>{
+        const user:User = await userValidator(context.req);
+        const {
+            room,
+            priceFrom = 0,
+            priceTo,
+            buildYearFrom = 0,
+            buildYearTo = 9999,
+            meterageFrom = 0,
+            meterageTo = 999999,
+            category=["Rent","Asker","Buyer","Sale"],
+            type=[
+                "Apartment",
+                "Pilot",
+                "Basement",
+                "Land",
+                "Resident",
+                "Eco",
+                "DailyRent",
+            ],
+        } = filter;
+
+        const house = await prisma.house.findMany({
+            where:{
+                AND:[
+                    {userId:user.id},
+                    {category:{in:category}}
+                    {...(room !== undefined && { room:{lte:room}})},
+                    {...(priceFrom !== undefined &&{price:{gte:priceFrom}})},
+                    {...(priceTo !== undefined &&{price:{lte:priceTo}})},
+                    {buildYear:{gte:buildYearFrom,lte:buildYearTo}},
+                    {meterage:{gte:meterageFrom,lte:meterageTo}},
+                    {type:{in:type}},
+                ]
+ 
+            }
+        });
+
+        if(house.length == 0) throw new Error('ملکی با این شرایط ثبت نکرده اید!')
+
+        return house;
+    },
+    singleHouse:async(_,{id})=>{
+        const house = await prisma.house.findUnique({where:{id}});
+        if(!house){
+            throw new Error('آگهی فروش یافت نشد!');
+        }else{
+            await prisma.house.update({where:{id},data:{watchCount:{increment:1}}});
+        }
+
+        return house;
+    },
+    deleteHouse:async(_,{id},context)=>{
+        const user:User = await userValidator(context.req);
+
+        const house = await prisma.house.delete({where:{id,userId:user.id}});
+        if(!house) throw new Error('حذف ملک با خطا مواجه شد!');
+        return `ملک ${house.title} با موفقیت حذف شد!`;
+    },
+    orderHouse:async(_,{category,type},context)=>{
+        let house = await prisma.house.findMany({
+            where:{AND:[
+                {...(category !== undefined && {category})},
+                {...(type !== undefined && {type})}
+            ]}
+        });
+
+        const filter = houseFilter(house);
+        return filter;
+    },
+};
+
+export const houseQuery = {
+    allHouse:async()=>{
+        const house = await prisma.house.findMany();
+        if(house.length == 0) throw new Error('ملکی در سایت ثبت نشده است!');
+        return house;
+    },
+    topViewedHouse:async()=>{
+        const house = (await prisma.house.findMany({})).sort((a,b)=>a.watchCount - b.watchCount);
+        if(house.length == 0){
+            throw new Error('ملکی در سایت موجود نیست');
+        }
+
+        return house;
+    },
+
+}
