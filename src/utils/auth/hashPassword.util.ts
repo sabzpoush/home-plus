30783015d79e53c292d76f2d6b4d8ce04a22bcd3
amir sabz <amirsabz84@gmail.com>
2025-01-24 02:33:05 +0330

    Update hashPassword.util.ts
    add new util for hashing password

--- src/utils/auth/hashPassword.util.ts
+++ src/utils/auth/hashPassword.util.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 
 export function hashPassword(password):string{
-        const salt = bcrypt.genSaltSync(10);
-        return bcrypt.hashSync(password,salt);
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password,salt);
 }
