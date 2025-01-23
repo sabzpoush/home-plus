32b90953fcb5b0d2546178c3bfea6b9a5dd0bcbf
amir sabz <50959230+sabzpoush@users.noreply.github.com>
2025-01-23 20:07:08 +0330

    chore : update util

--- src/utils/helper/filter.ts
+++ src/utils/helper/filter.ts
@@ -1,76 +1,6 @@
-import { Rent, Sale } from "@prisma/client";
+import { House } from "@prisma/client";
 
-export function saleFilter(sales){
-    const highToLowSales = [...sales].sort((s1, s2) => +s2.price - +s1.price);
-    const lowToHighSales = [...sales].sort((s1, s2) => +s1.price - +s2.price);
-    
-    const newestSales = [...sales].sort((s1, s2) => +s2.submitedAt - +s1.submitedAt);
-    const oldestSales = [...sales].sort((s1, s2) => +s1.submitedAt - +s2.submitedAt);
-
-    return {
-        highToLowSales,
-        lowToHighSales,
-        newestSales,
-        oldestSales,
-    };
-};
-
-export function rentfilter(rents,type = null){
-    if(type){
-        rents = rents.filter((rent)=> rent.type.toString() == type.toString());
-    }
-    const highToLowMortgage = [...rents].sort((s1, s2) => +s2.mortgage - +s1.mortgage);
-    const lowToHighMortgage = [...highToLowMortgage].reverse();
-    
-    const highToLowRent = [...rents].sort((s1, s2) => +s2.rent - +s1.rent);
-    const lowToHighRent = [...highToLowRent].reverse();
-    
-    const newestRent = [...rents].sort((s1, s2) => +s2.submitedAt - +s1.submitedAt);
-    const oldestRent = [...newestRent].reverse();
-
-    return {
-        highToLowMortgage,
-        lowToHighMortgage,
-        highToLowRent,
-        lowToHighRent,
-        newestRent,
-        oldestRent,
-    };
-}
-
-
-export function buyerFilter(buyers){
-    const newsetBuyer = [...buyers].sort((buyer1,buyer2)=> +buyer2.submitedAt - +buyer1.submitedAt);
-    const oldestBuyer = [...buyers].reverse();
-
-    const highToLowBuyers = [...buyers]
-        .filter((buyer)=> buyer.type.toString() == "Buyer")
-        .sort((buyer1,buyer2)=>buyer2.price - buyer1.price);
-    const lowToHighBuyers = [...highToLowBuyers].reverse();
-
-    const highToLowAskerRent = [...buyers]
-        .filter((buyer)=>buyer.type.toString() == "Asker")
-        .sort((buyer1,buyer2)=> +buyer2.rent - +buyer1.rent);
-    const lowToHighAskerRent = [...highToLowAskerRent].reverse();
-
-    const highToLowAskerMortgage = [...buyers]  
-        .filter((buyer)=>buyer.type.toString() == "Asker")
-        .sort((buyer1,buyer2)=> +buyer2.mortgage - +buyer1.mortgage);
-    const lowToHighAskerMortgage = [...highToLowAskerMortgage].reverse();
-
-    return {
-        newsetBuyer,
-        oldestBuyer,
-        highToLowBuyers,
-        lowToHighBuyers,
-        highToLowAskerRent,
-        lowToHighAskerRent,
-        highToLowAskerMortgage,
-        lowToHighAskerMortgage,
-    };
-};
-
-export function houseFilter(house){
+export function houseFilter(house:House[]){
     const newsetBuyer = [...house].sort((buyer1,buyer2)=> +buyer2.submitedAt - +buyer1.submitedAt);
     const oldestBuyer = [...house].reverse();
 
@@ -89,14 +19,20 @@ export function houseFilter(house){
         .sort((buyer1,buyer2)=> +buyer2.mortgage - +buyer1.mortgage);
     const lowToHighAskerMortgage = [...highToLowAskerMortgage].reverse();
 
-    const highToLowRentMortgage = [...house].sort((s1, s2) => +s2.mortgage - +s1.mortgage);
+    const highToLowRentMortgage = [...house]    
+        .filter((rent:House)=>rent.category.toString() === "Rent")
+        .sort((s1, s2) => +s2.mortgage - +s1.mortgage);
     const lowToHighRentMortgage = [...highToLowRentMortgage].reverse();
     
-    const highToLowRent = [...house].sort((s1, s2) => +s2.rent - +s1.rent);
+    const highToLowRent = [...house]
+        .filter((rent:House)=> rent.category.toString() === "Rent")
+        .sort((s1, s2) => +s2.rent - +s1.rent);
     const lowToHighRent = [...highToLowRent].reverse();
 
-    const highToLowSales = [...house].sort((s1, s2) => +s2.price - +s1.price);
-    const lowToHighSales = [...house].sort((s1, s2) => +s1.price - +s2.price);
+    const highToLowSales = [...house]
+        .filter((sale)=> sale.category.toString() == "Sale")
+        .sort((s1, s2) => +s2.price - +s1.price);
+    const lowToHighSales = [...house].reverse();
 
     return {
         newsetBuyer,
