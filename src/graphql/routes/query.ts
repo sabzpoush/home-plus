352139aa851814d1b085c4751dac96dc0cdde951
amir sabz <amirsabz84@gmail.com>
2025-01-26 00:02:47 +0330

    Update query and mutation

---
+++ src/graphql/routes/query.ts
@@ -0,0 +1,8 @@
+export const query = `#graphql
+    type Query{
+        allHouse:[House]
+        topViewedHouse:[House]
+        userPropertySubmited:[House]
+        likeProperty:[House]
+    }
+`;
