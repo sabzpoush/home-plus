33eedc1f6b04345fbc3150d001046955aa19e43a
amir sabz <50959230+sabzpoush@users.noreply.github.com>
2025-01-28 14:38:18 +0330

    Update house.validator.ts
    add filter input validator

--- src/utils/validator/house.validator.ts
+++ src/utils/validator/house.validator.ts
@@ -1,6 +1,6 @@
 import joi from 'joi';
 
-export const rentSchema = joi.object({
+export const houseValidator = joi.object({
     title: joi.string()
         .required()
         .min(3)
@@ -62,3 +62,25 @@ export const rentSchema = joi.object({
 });
 
 
+export const houseFilterValidator = joi.object({
+    priceFrom:joi.number(),
+    priceTo:joi.number(),
+    buildYearFrom:joi.number(),
+    buildYearTo:joi.number(),
+    meterageFrom:joi.number(),
+    metarageTo:joi.number(),
+   // type,
+    room:joi.number(),
+}).custom((value, helpers) => {
+    if (value.priceFrom > value.priceTo) {
+        return helpers.message({message:'شروع قیمت نمی تواند از سقف قیمت کمتر باشد!'});
+      }
+    if (value.meterageFrom > value.metarageTo) {
+      return helpers.message({message:'متر اولیه نمی تواند کمتر از متر ثانویه باشد!'});
+    }
+    if (value.buildYearFrom > value.buildYearTo) {
+        return helpers.message({message:'سال ساخت اولیه نمی تواند بیشتر از سال ساخت ثانویه باشد!'});
+    }
+
+    return value;
+});;
