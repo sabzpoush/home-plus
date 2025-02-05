2004666b7470ea7baa8646e22395ddf2400d2eb9
amir sabz <amirsabz84@gmail.com>
2025-02-05 14:26:53 +0330

    Update user.validator.ts
    add login & register validator

--- src/utils/validator/user.validator.ts
+++ src/utils/validator/user.validator.ts
@@ -1,6 +1,12 @@
 import joi from 'joi';
 
 export const userValidator = joi.object({
-    email:joi.string().email(),
-    password: joi.string().min(8),
+    email:joi.string().required().message('لطفا ایمیل خود را وارد کنید!').email().message('ایمیل اشتباه است!'),
+    password: joi.string()
+        .required()
+        .message('رمز عبور خود را وارد کنید!')
+        .min(8)
+        .message('رمز عبور شما نمی تواند کمتر از ۸ کاراکتر باشد!')
+        .max(255)
+        .message('رمز عبور شما نمی تواند بیش از ۲۵۵ کاراکتر باشد!'),
 });
